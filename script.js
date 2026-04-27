5284c273df1936939897d4918890a68b658f16c9
ratiratirati <43688993+ratiratirati@users.noreply.github.com>
2026-04-27 23:21:50 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ function scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" });
 }
 
+window.addEventListener("scroll", function () {
+    const btn = document.getElementById("topBtn");
+
+    if (!btn) return;
+
+    if (window.scrollY > 200) {
+        btn.style.display = "block";
+    } else {
+        btn.style.display = "none";
+    }
+});
+
 function setLang(lang) {
 
     localStorage.setItem("lang", lang);
